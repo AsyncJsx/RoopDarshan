c6db7daf0fe7b5470fea960d1811f3ddff39e27a
siddiqueshaikh <143960810+shaikhsiddique@users.noreply.github.com>
2026-03-29 22:09:32 +0530

    Status code corrected

--- Backend/controller/product.controller.js
+++ Backend/controller/product.controller.js
@@ -226,11 +226,11 @@ const getDataController = async (req,res)=>{
     return res.status(404).json({ success: false, message: "Product not found",error:error.message });
   }
 }
-
+// status code correct
 const getAllProductsController = async (req,res)=>{
   try {
     const products = await productService.getAllProducts();
-    return res.status(200).json({ success: true,products, message: "Products found Successfully" });
+    return res.status(302).json({ success: true,products, message: "Products found Successfully" });
   } catch (error) {
     return res.status(404).json({ success: false, message: "Products not found",error:error.message });
   }
